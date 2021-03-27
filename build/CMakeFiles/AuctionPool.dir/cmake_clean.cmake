cfa3e8200786ba23c4c55a3cf62dbd948357ed29
Tosin Kuye <tkuye77@gmail.com>
2021-03-27 17:41:58 -0600

    Created cmake project and directory

---
+++ build/CMakeFiles/AuctionPool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AuctionPool"
+  "AuctionPool.pdb"
+  "CMakeFiles/AuctionPool.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AuctionPool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
